025b6d375ff7a30366e7c672d5d509b9ac8241f8
bcarlso1 <bcarlso1@gmail.com>
2019-11-30 23:22:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ var mobileChart = new Chart(ctx, {
       datasets: [{
           data: [67, 18, 15],
           backgroundColor: [
-              'purple',
-              'green',
+            '#8080ff',
+              'limegreen',
               'teal'
           ],
           borderColor: [
@@ -92,3 +92,144 @@ var mobileChart = new Chart(ctx, {
       }
   }
 });
+
+
+
+// ************
+// REMOVE ALERT
+// ************
+
+document.getElementById('alert').addEventListener('click', myFunction);
+
+function myFunction() {
+     var chartDiv = document.getElementById('charts');
+     chartDiv.removeChild(chartDiv.childNodes[6]);
+}; 
+
+// ************
+// SEND MESSAGE
+// ************
+
+function sendMessage() {
+    alert("Your message was sent.  We will get back to you soon!");
+    document.getElementById("send-message").onsubmit();
+   
+}
+
+function messageValidate() {
+    var messageCheck = document.forms["sendMessage"]["textbox"].value;
+    if (messageCheck == "") {
+        alert("Message is blank");
+        return false;
+    }
+    var nameCheck = document.forms["sendMessage"]["userSearch"].value;
+    if (nameCheck == "") {
+        alert("User not selected");
+        return false;
+    }
+    else {
+        alert("Your message was sent.  We will get back to you soon!");
+        return true;
+    }
+}
+
+// ************
+// BELL NOTIFICATIONS
+// ************
+
+document.getElementById('bell').addEventListener('click', ringBell);
+
+function ringBell() {
+     alert("Tom commented on your photo" + "\n" + "Bert liked your photo")
+}; 
+
+// **************
+// SWITCH CHARTS
+// **************
+
+document.getElementById('monthly').addEventListener('click', showMonthly);
+
+    function showMonthly() {
+        clearColor();
+        document.getElementById('monthly').style.backgroundColor = "limegreen"; 
+        removeCurrentChart();
+    }
+
+document.getElementById('daily').addEventListener('click', showDaily);
+
+    function showDaily() {
+        clearColor();
+        document.getElementById('daily').style.backgroundColor = "limegreen";
+        removeCurrentChart();
+    }
+
+document.getElementById('weekly').addEventListener('click', showWeekly);
+
+    function showWeekly() {
+        clearColor();
+        document.getElementById('weekly').style.backgroundColor = "limegreen";
+        removeCurrentChart();
+    }
+
+document.getElementById('hourly').addEventListener('click', showHourly);
+
+    function showHourly() {
+        clearColor();
+        document.getElementById('hourly').style.backgroundColor = "limegreen";
+        removeCurrentChart();
+    }
+
+
+
+function clearColor() {
+    var nodeA = document.getElementById('traffic').children;
+    for (var i = 0; i < nodeA.length; i++) {
+       nodeA[i].style.backgroundColor = "white";
+       }
+}; 
+
+
+function removeCurrentChart() {
+    var trafficDiv = document.getElementById('traffic');
+    var trafficChartDiv = document.getElementById('chartSpot');
+    var trafficGone = trafficDiv.removeChild(trafficChartDiv);
+}
+
+// **********************
+// SEARCH AUTOCOMPLETE
+// **********************
+
+var names = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"]
+
+
+
+
+var entry = document.getElementById('searchBar');
+
+entry.addEventListener('keyup', autoComplete);
+
+function autoComplete() {
+    entry.value = entry.value.toLowerCase();
+    var myEntry = entry.value;
+    console.log(myEntry);
+}
+
+/* start
+var items = document.getElementsByTagName('a');
+var itemsParent = document.getElementsByClassName('flex-item')
+
+
+
+function myFunction() {
+
+    for (var i = 0; i < items.length; i += 1) {
+        var title = items[i].getAttribute('data-caption');
+        console.log(title);
+        if (title.indexOf(myEntry) > -1) {
+            itemsParent[i].style.display = "";
+        } else {
+            itemsParent[i].style.display = "none";
+        }
+}
+}
+end */
